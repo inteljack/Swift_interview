456b3446d5b5d5a9cfb622352d496aa741112bb1
Ying-Ta Lin <ytl287@nyu.edu>
2017-03-07 19:14:37 -0500

    scan and correct with pep8 style guide

--- src/main/Fizzbuzz.py
+++ src/main/Fizzbuzz.py
@@ -9,9 +9,12 @@ if the number is divisible by 3 and 5 (15) print FizzBuzz;
 else, print the number.
 
 """
+
+
 class Swift(object):
-	def fizzbuzz(self, n):
-		return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
+    def fizzbuzz(self, n):
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i)
+                for i in range(1, n+1)]
 
 res = Swift()
-print res.fizzbuzz(100) # demonstrate with number 100
+print res.fizzbuzz(100)  # demonstrate with number 100
