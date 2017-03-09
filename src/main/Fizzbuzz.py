47a3e352ecf4df29613483a8e1a6699802236ab1
Ying-Ta Lin <ytl287@nyu.edu>
2017-03-09 17:51:50 -0500

    Fixed linting error

--- src/main/Fizzbuzz.py
+++ src/main/Fizzbuzz.py
@@ -12,9 +12,20 @@ else, print the number.
 
 
 class Swift(object):
-    def fizzbuzz(self, n):
-        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i)
-                for i in range(1, n+1)]
+    """
+    Usage: example = Swift()
+    class takes no input
+    """
+    def __init__(self):
+        pass
 
-res = Swift()
-print res.fizzbuzz(100)  # demonstrate with number 100
+    @classmethod
+    def fizzbuzz(cls, num):
+        """
+        Usage: Swift().fizzbuzz(number of testing numbers from 1)
+        """
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i)
+                for i in range(1, num+1)]
+
+result = Swift()
+print result.fizzbuzz(100)  # demonstrate with number 100
